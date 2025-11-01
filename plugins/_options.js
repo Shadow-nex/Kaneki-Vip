2d70b7752a4620c05d8ef7ddaad069604966dab2
the-27 <blackoficial2025@gmail.com>
2025-11-01 18:15:01 -0500

    🍓 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 -- 𝐬𝐡𝐚𝐝𝐨𝐰.𝐱𝐲𝐳 🍃

---
+++ plugins/_options.js
@@ -0,0 +1,136 @@
+const handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin }) => {
+const primaryBot = global.db.data.chats[m.chat].primaryBot
+if (primaryBot && conn.user.jid !== primaryBot) throw !1
+const chat = global.db.data.chats[m.chat]
+let type = command.toLowerCase()
+let isEnable = chat[type] !== undefined ? chat[type] : false
+if (args[0] === 'on' || args[0] === 'enable') {
+if (isEnable) return conn.reply(m.chat, `🪺 *${type}* ya estaba *activado*.`, m, rcanal)
+isEnable = true
+} else if (args[0] === 'off' || args[0] === 'disable') {
+if (!isEnable) return conn.reply(m.chat, `🪵 *${type}* ya estaba *desactivado*.`, m, rcanal)
+isEnable = false
+} else {
+return conn.reply(m.chat, `*_╭━━━⊜ ⌊• \`ᴘᴀɴᴇʟ ᴅᴇ ᴄᴏɴᴛʀᴏʟ\` •⌉_*
+*_┃🍃❏ Un administrador puede gestionar el comando:_*
+*_┃_* ╰➤ *${command}*
+*_┃_*
+*_┃_* \`🪄 Opciones disponibles: 🌾\`
+*_┃_* 𖥔 _Activar_ » *${usedPrefix}${command} enable*  
+*_┃_* 𖥔 _Desactivar_ » *${usedPrefix}${command} disable*  
+*_┃_*  
+*_┃_* \`🌿 Estado actual:\`
+*_┃_* ╰➤ *${isEnable ? '✓ Activado' : '✗ Desactivado'}*
+*_╰━━━━━━━━━━━━━━━━⊜_*`, m, rcanal)
+}
+switch (type) {
+case 'welcome': case 'bienvenida': {
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}} else if (!isAdmin) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.welcome = isEnable
+break
+}
+case 'modoadmin': case 'onlyadmin': {
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}} else if (!isAdmin) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.modoadmin = isEnable
+break
+}
+case 'detect': case 'alertas': {
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}} else if (!isAdmin) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.detect = isEnable
+break
+}
+case 'antilink': case 'antienlace': {
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}} else if (!isAdmin) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.antiLink = isEnable
+break
+}
+case 'nsfw': case 'modohorny': {
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}} else if (!isAdmin) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.nsfw = isEnable
+break
+}
+case 'economy': case 'economia': {
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}} else if (!isAdmin) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.economy = isEnable
+break
+}
+case 'rpg': case 'gacha': {
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}} else if (!isAdmin) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.gacha = isEnable
+break
+}}
+chat[type] = isEnable
+conn.reply(m.chat, `*_🕸️ Has \`${isEnable ? 'activado' : 'desactivado'}\` el \`${type}\` para este grupo._*`, m, rcanal)
+}
+
+handler.help = [
+  'welcome', 'bienvenida',
+  'modoadmin', 'onlyadmin',
+  'nsfw', 'modohorny',
+  'economy', 'economia',
+  'rpg', 'gacha', 
+  'detect', 'alertas',
+  'antilink', 'antienlace',
+  'antilinks', 'antienlaces']
+handler.tags = ['nable']
+handler.command = [
+  'welcome', 'bienvenida',
+  'modoadmin', 'onlyadmin',
+  'nsfw', 'modohorny',
+  'economy', 'economia',
+  'rpg', 'gacha', 
+  'detect', 'alertas',
+  'antilink', 'antienlace',
+  'antilinks', 'antienlaces']
+handler.group = true
+
+export default handler
